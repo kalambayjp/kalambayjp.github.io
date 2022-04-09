2fca6683b74f050630ceb66a074520cbd5178966
Jp <kalambayjp@gmail.com>
2022-04-09 10:57:53 -0700

    updates in about paragraphs

--- src/Components/About/index.jsx
+++ src/Components/About/index.jsx
@@ -4,23 +4,21 @@ const About = () => {
       <div className="text-zone">
         <h1>About Me</h1>
         <p>
-          I am a full-stack developer, looking for an entry-level position at a
-          great company to be a hard-working asset to the team, to learn, grow
-          and develop long-term. I am proficient in JavaScript, HTML, CSS, SQL
-          and testing frameworks such as Jest, Mocha and cypress.
+          I am a full-stack developer who is proficient in JavaScript, HTML,
+          CSS, SQL and testing frameworks such as Jest, Mocha and cypress.
         </p>
 
         <p>
-          I am currently using my talents as a freelance developer building
-          sites for small local companies and as a part-time mentor, helping
-          coding bootcamp students at Lighthouse Labs. Prior to this I developed
-          several apps, some of which I have highlighted in the projects page.
+          I am currently working as a freelance developer building sites for
+          small local companies and as a part-time mentor, helping coding
+          bootcamp students at Lighthouse Labs. I am also on the look out for
+          full-time opportunities. Prior to this I developed several apps, some
+          of which I have highlighted in the projects page.
         </p>
 
         <p>
-          I am quitely confident, goal driven and love to find new way to
-          challenge myself. Outside coding my interests include exercise and
-          nutrition, sports and aviation.
+          Outside coding my interests include exercise and nutrition, sports and
+          aviation.
         </p>
       </div>
     </div>
